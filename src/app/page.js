686a07cb6f1a340c5b509a624fab383453de0395
Pive0528 <pive0528@gmail.com>
2024-09-30 11:41:07 +0900

    MUI 테마 커스터마이징, React Icons, MUI App Bar 컴포넌트

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,29 @@
 'use client';
-import Button from '@mui/material/Button';
+
+import * as React from 'react';
+import { AppBar, Toolbar } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+import theme from './theme';
 
 export default function Home() {
   return (
     <>
-      <button>버튼</button>
-      <Button className="tw-bg-red-500" variant="contained">
-        Contained
-      </Button>
+      {/* <AppBar position="static"> */}
+      <AppBar position="fixed">
+        <Toolbar>
+          <div className="tw-flex-1">
+          </div>
+          <div className="logo-box">
+            <a href="/" className="tw-font-bold">
+              로고
+            </a>
+          </div>
+          <div className="tw-flex-1 tw-flex tw-justify-end">글쓰기</div>
+        </Toolbar>
+      </AppBar>
+      <section className="tw-h-screen tw-flex tw-items-center tw-justify-center tw-text-[2rem]">
+        section
+      </section>
     </>
   );
 }
